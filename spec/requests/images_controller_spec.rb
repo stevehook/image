a7e23479b1b65832d01c553d5d02dea5e0d68920
Steve Hook <steve.hook@gmail.com>
2019-02-05 21:44:51 +0000

    Failing spec asserting create action actually creates something

--- spec/requests/images_controller_spec.rb
+++ spec/requests/images_controller_spec.rb
@@ -15,5 +15,11 @@ RSpec.describe ImagesController, type: :request do
       post images_url, params: { image: { name: 'red', file: file } }
       expect(response).to be_success
     end
+
+    it 'creates an image record' do
+      expect do
+        post images_url, params: { image: { name: 'red', file: file } }
+      end.to change { Image.count }.by(1)
+    end
   end
 end

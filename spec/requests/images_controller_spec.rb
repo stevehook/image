7bba36b0f205fb4bb49208c01693e2d07f115844
Steve Hook <steve.hook@gmail.com>
2019-02-05 21:43:14 +0000

    Make initial spec assert something vaguely useful

--- spec/requests/images_controller_spec.rb
+++ spec/requests/images_controller_spec.rb
@@ -3,13 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe ImagesController, type: :request do
+  let(:file) do
+    fixture_file_upload(
+      Rails.root.join('spec', 'fixtures', 'red.jpg'),
+      'image/jpg'
+    )
+  end
+
   describe '#create' do
     it 'returns success' do
-      file = fixture_file_upload(
-        Rails.root.join('spec', 'fixtures', 'red.jpg'),
-        'image/jpg'
-      )
       post images_url, params: { image: { name: 'red', file: file } }
+      expect(response).to be_success
     end
   end
 end
